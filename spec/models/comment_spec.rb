da9c39e085681c39c1ba3508d756f7a0364fa4d4
Aleksandr Klimenkov <prisioner@mail.ru>
2017-09-25 16:19:48 +0300

    add comment model spec

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  describe 'validations check' do
+    it { should belong_to :event }
+    it { should belong_to :user }
+
+    it { should validate_presence_of :body }
+
+    # user is optional
+    it { should allow_value(nil).for(:user) }
+
+    context 'user is present' do
+      subject { Comment.new(user: FactoryGirl.create(:user)) }
+      it { should_not validate_presence_of :user_name }
+    end
+
+    context 'user is nil' do
+      it { should validate_presence_of :user_name }
+    end
+  end
+
+  describe '#user_name' do
+    context 'user is present' do
+      let(:user) { FactoryGirl.create(:user, name: 'Петя') }
+      let(:comment) { FactoryGirl.create(:comment_with_author, user: user, user_name: 'Вася') }
+
+      it 'returns name of registered user' do
+        expect(comment.user_name).to eq 'Петя'
+      end
+    end
+
+    context 'user is blank' do
+      let(:comment) { FactoryGirl.create(:comment, user_name: 'Анонимус') }
+
+      it 'returns user_name' do
+        expect(comment.user_name).to eq 'Анонимус'
+      end
+    end
+  end
+end
