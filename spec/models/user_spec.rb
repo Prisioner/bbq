6db9ca739bb4d7884c9dd2b600d67e4a50e242e8
Aleksandr Klimenkov <prisioner@mail.ru>
2017-09-26 01:26:38 +0300

    add user model spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'associations and validations check' do
+    it { should have_many :events }
+    it { should have_many :comments }
+    it { should have_many :photos }
+    it { should have_many :subscriptions }
+
+    it { should validate_presence_of :name }
+    it { should validate_length_of(:name).is_at_most(35) }
+  end
+
+  describe '#link_subscriptions' do
+    let!(:subscription1) { FactoryGirl.create(:subscription, user_email: 'user123@example.com') }
+    let!(:subscription2) { FactoryGirl.create(:subscription, confirmed: false, user_email: 'user123@example.com') }
+    let!(:user) { FactoryGirl.build(:user, email: 'user123@example.com') }
+
+    it 'confirms not-confirmed subscription' do
+      user.save
+      subscription2.reload
+
+      expect(subscription2).to be_confirmed
+    end
+
+    it 'sends an email only when confirms subscriptions' do
+      expect { user.save }.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+
+    it 'links subscriptions to user' do
+      user.save
+
+      expect(user.subscriptions).to contain_exactly(subscription1, subscription2)
+    end
+  end
+end
