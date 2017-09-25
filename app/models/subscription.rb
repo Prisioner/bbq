70ecd7057b74a3952715f4675fe3a22a21eaa0a6
Aleksandr Klimenkov <prisioner@mail.ru>
2017-09-25 15:28:17 +0300

    refactor conditions, string -> block

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,15 +2,15 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user_name, presence: true, unless: 'user.present?'
-  validates :user_email, presence: true, format: { with: /\A[a-z\d\-_.]+@[a-z\d\-_.]+\z/i }, unless: 'user.present?'
+  validates :user_name, presence: true, unless: -> { user.present? }
+  validates :user_email, presence: true, format: { with: /\A[a-z\d\-_.]+@[a-z\d\-_.]+\z/i }, unless: -> { user.present? }
 
-  validates :user, uniqueness: { scope: :event_id }, if: 'user.present?'
-  validates :user_email, uniqueness: { scope: :event_id }, unless: 'user.present?'
+  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
+  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
   validates :confirm_token, uniqueness: { case_sensitive: false }, allow_nil: true
-  validate :email_taken, unless: 'user.present?'
+  validate :email_taken, unless: -> { user.present? }
 
-  before_validation :user_email_downcase, unless: 'user.present?'
+  before_validation :user_email_downcase, unless: -> { user.present? }
   before_create :confirmation
 
   scope :confirmed, -> { where(confirmed: true) }
